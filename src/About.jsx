cbe852e809988f5c60cb5a4ff28c2f51922f4f60
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-02-15 01:20:01 +0000

    About component

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,8 @@
 export const About = () => {
   return (
-      <main>
-          <h1>About</h1>
+      <main className="About">
+          <h2>About</h2>
+          <p style={{marginTop: "1rem"}}>This is a project to lenar React</p>
       </main>
   )
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   const today = new Date();
   return (
       <footer className="Footer">
-          <p>Copyright &copy; {today.getFullYear}</p>
+          <p>Copyright &copy; {today.getFullYear()}</p>
       </footer>
   )
 }
