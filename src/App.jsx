203fbf04cd88d6ae4931ec83437943e994650c73
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-02-21 18:52:56 +0000

    useAxiosFetch class crate

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import NewPost from './NewPost'
 import PostPage from './PostPage'
 import api from './api/posts'
 import { format } from 'date-fns'
+import useAxiosFetch from './hooks/useAxiosFetch'
 import useWindowSize from './hooks/useWindowSize'
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
     const [findPost, setFindPost] = useState([]);
     const history = useHistory()
     const {width} = useWindowSize();
+    const {data, fetchError, isLoading} = useAxiosFetch('http://localhost:3500/posts');
 
     useEffect(() => {
         const fetchPosts = async () => {
---
+++ src/hooks/useAxiosFetch.jsx
@@ -0,0 +1,47 @@
+import {useEffect, useState} from 'react'
+
+import axios from 'axios'
+
+const useAxiosFetch = (dataUrl) => {
+  const [data,setData] = useState([])
+  const [fetchError, setFetchError] = useState(null)
+  const [isLoading, setIsLoading] = useState(null)
+
+  useEffect(()=> {
+    let isMounted = true
+    const source = axios.CancelToken.source();
+
+    const fetchData = async (url) => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(url, {
+          cancelToken: source.token
+        });
+        if(isMounted) {
+          setData(response.data)
+          setFetchError(null);
+        }
+      } catch (error) {
+        if(isMounted) {
+          setFetchError(error.message);
+          setData([]);
+        }
+        
+      }finally{
+        isMounted && setTimeout(() => setIsLoading(false, 2000));
+      }
+    }
+    fetchData(dataUrl);
+
+    const cleanUp = () => {
+      console.log('clean up function')
+      isMounted = false;
+      source.cancel();
+    }
+    return cleanUp;
+
+  }, [dataUrl])
+  return {data, fetchError, isLoading}
+}
+
+export default useAxiosFetch
